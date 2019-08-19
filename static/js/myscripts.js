5430a802331e1e8ec020978d1f3070820d1c396e
anzo559 <anzo559@gmail.com>
2019-08-19 18:50:51 +0200

    Add cookies alert

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -10,3 +10,5 @@
 </script>
 
 <script src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" type="text/javascript"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"></script>
